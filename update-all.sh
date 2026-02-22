eaea023cd9642e94025dfe14e71574c69ae126ef
copiiinux <loick2610@gmail.com>
2026-02-22 18:25:23 +0000

    Improved update script
    Signed-off-by: copiiinux <loick2610@gmail.com>

---
+++ update-all.sh
@@ -0,0 +1,14 @@
+git pull
+
+for dir in */; do
+  compose_file="$dir/compose.yaml"
+  env_file="$dir/.env"
+
+  if [[ -f "$compose_file" ]]; then
+    cd "$dir" || continue
+    docker compose up --build -d --pull=always --remove-orphans --wait --wait-timeout 60 -y
+    cd - > /dev/null
+  fi
+done
+
+docker system prune -a --volumes -f
